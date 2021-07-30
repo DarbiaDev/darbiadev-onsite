b86a1b6052a2dbf47fda369920751f4fa7d0d640
Bradley Reynolds <bradley.reynolds@darbia.dev>
2021-07-29 19:49:43 -0500

    Docs were nested?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,7 +20,7 @@ import os
 import sys
 import toml
 
-sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
